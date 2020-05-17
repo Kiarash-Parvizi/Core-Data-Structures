fe98c7b9c86248d3bc66fbcd284e498453554f43
Kiarash-Parvizi <cdev.developer@gmail.com>
2020-05-17 21:48:46 +0430

    MemoryTree update

--- MemoryTree/MemoryTree.hpp
+++ MemoryTree/MemoryTree.hpp
@@ -21,18 +21,58 @@ class MemoryTree {
 	};
 	// data vec
 	vector<MemoryNode> nodes;
+	vector<int> removedNodes;
 	
 public:
 	MemoryTree(int res_size = 256) {
 		nodes.reserve(res_size);
+		removedNodes.reserve(64);
+	}
+
+	inline int size() {
+		return nodes.size();
 	}
 
 	// parent_id<0 : adds new node directly to the root
 	const int emplace_child(const int parent_id, const T child_val) {
+		// check for place in deleted Nodes
+		if(removedNodes.size()) {
+			auto id = removedNodes[removedNodes.size()-1];
+			removedNodes.pop_back();
+			for (const auto& v : nodes[id].childs) {
+				removedNodes.emplace_back(v);
+			}
+			//replace
+			auto& node = nodes[id];
+			node.val = child_val;
+			node.childs.clear();
+			node.parent = parent_id;
+			//link-par
+			if (parent_id>=0)
+				nodes[parent_id].childs.push_back(id);
+			return id;
+		}
+		//
 		nodes.emplace_back(MemoryNode(parent_id, child_val));
+		//link-par
 		if (parent_id>=0)
 			nodes[parent_id].childs.push_back(nodes.size()-1);
 		return nodes.size()-1;
+	}
+
+	const int emplace_childs(const int parent_id, const vector<T>& childs) {
+		int id = parent_id;
+		for (const auto& v : childs) {
+			id = emplace_child(id, v);
+		}
+		return id;
+	}
+
+	void remove(const int id) {
+		if (id < 0 || id >= nodes.size()) {
+			throw std::exception();
+		}
+		removedNodes.emplace_back(id);
 	}
 
 	bool canBranchFind(const int leaf_id, const T val) const {
--- usage/usage_MemoryTree.cpp
+++ usage/usage_MemoryTree.cpp
@@ -28,10 +28,44 @@ void u2() {
 	cout << "can find data in selected branch: " << tree.canBranchFind(child_id2, {1,2,"second"}) << '\n';
 }
 
+void u3() {
+	MemoryTree<testStruct> tree;
+	auto child_id0 = tree.emplace_child(-1, {1,2,"f"});
+	auto child_id1 = tree.emplace_child(child_id0, {1,3,"s"});
+	auto child_id  = tree.emplace_child(-1, {1,4,"f"});
+	auto child_id2 = tree.emplace_childs(child_id1, {{2,3,"A"}, {2,3,"B"}, {2,3,"C"}, {2,3,"D"}});
+	cout << "can find data in selected branch: " << tree.canBranchFind(child_id2, {2,3,"A"}) << '\n';
+	cout << "can find data in selected branch: " << tree.canBranchFind(child_id2, {2,3,"C"}) << '\n';
+}
+
+void u4() {
+	MemoryTree<int> tree;
+	auto i1 = tree.emplace_child(-1, 1);
+	auto i2 = tree.emplace_child(-1, 22);
+	auto i3 = tree.emplace_child(i1, 3);
+	auto i4 = tree.emplace_child(i3, 4);
+	cout << "can find data in selected branch: " << tree.canBranchFind(i4, 1) << '\n';
+	cout << "tree size: " << tree.size() << '\n';
+	auto I2 = i2;
+	tree.remove(i1);
+	i1 = tree.emplace_child(-1, 1);
+	i2 = tree.emplace_child(-1, 2);
+	i3 = tree.emplace_child(i1, 3);
+	i4 = tree.emplace_child(i3, 4);
+	cout << "tree size: " << tree.size() << '\n';
+	cout << "can find data in selected branch: " << tree.canBranchFind(I2, 22) << '\n';
+}
+
 int main() {
 	cout << "u1:\n";
 	u1();
 	cout << "---------------\n";
 	cout << "u2:\n";
 	u2();
+	cout << "---------------\n";
+	cout << "u3:\n";
+	u3();
+	cout << "---------------\n";
+	cout << "u4:\n";
+	u4();
 }
